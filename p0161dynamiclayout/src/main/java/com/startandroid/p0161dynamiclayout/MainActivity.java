09378310a03bb67b3c7809c7baeb708e2ef1d112
AbadzhevaK8 <make.me.loco@gmail.com>
2022-12-26 18:23:41 +0300

    doc: add lesson 15.

---
+++ p0161dynamiclayout/src/main/java/com/startandroid/p0161dynamiclayout/MainActivity.java
@@ -0,0 +1,24 @@
+package com.startandroid.p0161dynamiclayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+
+        // создание LinearLayout
+        LinearLayout linLayout = new LinearLayout(this);
+        // установим вертикальную ориентацию
+        linLayout.setOrientation(LinearLayout.VERTICAL);
+        // создаем LayoutParams
+        LinearLayout.LayoutParams linLayoutParam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        // устанавливаем linLayout как корневой элемент экрана
+        setContentView(linLayout, linLayoutParam);
+    }
+}
